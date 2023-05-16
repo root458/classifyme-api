31c5c7762d06b4634028a0dde4b3357195e453da
Hope Kagame <hopekagame1@gmail.com>
2023-05-16 16:03:46 +0300

    Add accept headers

--- main.py
+++ main.py
@@ -169,5 +169,12 @@ def middleware():
     # new_headers['Accept'] = 'application/json'
     # request.headers = new_headers
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
 if __name__ == "__main__":
     app.run()
